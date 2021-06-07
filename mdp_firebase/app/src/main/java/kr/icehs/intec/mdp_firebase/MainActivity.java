37c98622c5dd8ca03c5d9e1398168f275851b023
whtkddus91 <84482598+whtkddus91@users.noreply.github.com>
2021-06-08 08:32:37 +0900

    create main prototype

--- mdp_firebase/app/src/main/java/kr/icehs/intec/mdp_firebase/MainActivity.java
+++ mdp_firebase/app/src/main/java/kr/icehs/intec/mdp_firebase/MainActivity.java
@@ -20,29 +20,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mFirebaseAuth = FirebaseAuth.getInstance();
-        btn_logout = findViewById(R.id.btn_logout);
-        btn_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 로그아웃 하기
-                mFirebaseAuth.signOut();
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        btn_delete = findViewById(R.id.btn_delete);
-        btn_delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 회원 탈퇴
-                mFirebaseAuth.getCurrentUser().delete();
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+//        btn_logout = findViewById(R.id.btn_logout);
+//        btn_logout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // 로그아웃 하기
+//                mFirebaseAuth.signOut();
+//                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
+//
+//        btn_delete = findViewById(R.id.btn_delete);
+//        btn_delete.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // 회원 탈퇴
+//                mFirebaseAuth.getCurrentUser().delete();
+//                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
 
     }
 }
